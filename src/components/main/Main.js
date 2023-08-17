028067363ddd409b43e9165d1bbc38528e7d9589
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-17 11:41:13 +0300

    makes left container a column and also puts the custom button in it

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -10,9 +10,9 @@ const Main = () => {
             <div className="first-section-left-container">
                 <h1>More than just <span>shorter links</span></h1>
                 <p>Build your brands recognition and get detailed <span>insights on how your links are performing</span></p>
+                <Button buttonText={"Get Started"} border={"2px solid hsl(180, 66%, 49%)"} backgroundColor={"hsl(180, 66%, 49%)"} color={"white"} borderRadius={"25px"}/>
             </div>
-            <div className="first-section-right-container"></div>
-            <Button buttonText={"Get Started"} border={"2px solid hsl(180, 66%, 49%)"} backgroundColor={"hsl(180, 66%, 49%)"} color={"white"} borderRadius={"25px"}/>
+            <div className="first-section-right-container"></div> 
             <div className="first-section-right-container">
                 <img src={IllustrationWorking} alt="illustration-hero" />
             </div>
