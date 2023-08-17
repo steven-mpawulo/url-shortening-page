536ac0432049587ed9446f93ba990720fd982e59
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-17 11:29:59 +0300

    writes html for first section left container

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import Button from '../common/button/Button'
 
 const Main = () => {
   return (
     <main>
         <section className='first-section'>
-            <div className="first-section-left-container"></div>
+            <div className="first-section-left-container">
+                <h1>More than just <span>shorter links</span></h1>
+                <p>Build your brands recognition and get detailed <span>insights on how your links are performing</span></p>
+            </div>
             <div className="first-section-right-container"></div>
+            <Button buttonText={"Get Started"} border={"2px solid hsl(180, 66%, 49%)"} backgroundColor={"hsl(180, 66%, 49%)"} color={"white"} borderRadius={"25px"}/>
         </section>
     </main>
   )
