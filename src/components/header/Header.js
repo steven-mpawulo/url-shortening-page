7fb2f2be5db6b52c87024a956538002006007dfc
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-17 16:01:47 +0300

    wraps the buttons in header in a container

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -14,8 +14,10 @@ const Header = () => {
             <li><a href="#Resources">Resources</a></li>
         </ul>
         </nav>
+        <div className="container-for-button">
         <Button buttonText={"Login"} border={"none"} backgroundColor={"white"} color={"hsl(0, 0%, 75%)"}/>
         <Button buttonText={"Sign Up"} border={"2px solid hsl(180, 66%, 49%)"} backgroundColor={"hsl(180, 66%, 49%)"} color={"white"} borderRadius={"25px"}/>
+        </div>
 
     </header>
   )
