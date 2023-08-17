c9629fa0fdde2b51e503a20239afdcf86715658d
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-17 12:55:05 +0300

    styles card

--- src/components/common/card/Card.js
+++ src/components/common/card/Card.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import './card.css'
 
 const Card = ({icon, title, message}) => {
   return (
     <div className="card">
+        <div className="container-for-icon">
         <img src={icon} alt="icon"  className='icon'/>
+        </div>
         <h3>{title}</h3>
         <p>{message}</p>
     </div>
