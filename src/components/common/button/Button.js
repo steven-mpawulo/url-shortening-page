34d00f26b6030e20b24e4e3272a5a9ba52233a1e
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-17 11:08:36 +0300

    modfies border

--- src/components/common/button/Button.js
+++ src/components/common/button/Button.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({ buttonText, color, backgroundColor, borderRadius }) => {
     return (
-        <button style={{ color: `${color}`, backgroundColor: `${backgroundColor}`, border: `2px solid ${backgroundColor}`, borderRadius: `${borderRadius}` }} className='custom-button'>{buttonText}</button>
+        <button style={{ color: `${color}`, backgroundColor: `${backgroundColor}`, border: `${border}`, borderRadius: `${borderRadius}` }} className='custom-button'>{buttonText}</button>
     )
 }
 
