a79c36a8994fee2a00100dff22414c4970c266a5
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-17 10:40:05 +0300

    adds logo and nav element

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import Logo from '../../images/logo.svg'
 
 const Header = () => {
   return (
     <header>
+        <img src={Logo} alt="logo" />
+        <nav>
+        <ul>
+            <li><a href="#Features">Features</a></li>
+            <li><a href="#Pricing"></a>Pricing</li>
+            <li><a href="#Resources"></a>Resources</li>
+        </ul>
+        </nav>
 
     </header>
   )
