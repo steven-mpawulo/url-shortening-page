6115ee4e8797929dd5c9f9af6f26b7a7f4095e18
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-17 14:01:30 +0300

    modifies card to make it possible to position itself

--- src/components/common/card/Card.js
+++ src/components/common/card/Card.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import './card.css'
 
-const Card = ({icon, title, message}) => {
-  return (
-    <div className="card">
-        <div className="container-for-icon">
-        <img src={icon} alt="icon"  className='icon'/>
+const Card = ({ icon, title, message, position, pushedFromTop }) => {
+    return (
+        <div className="card" style={{ position: `${position}`, top: `${pushedFromTop}` }}>
+            <div className="container-for-icon">
+                <img src={icon} alt="icon" className='icon' />
+            </div>
+            <h3>{title}</h3>
+            <p>{message}</p>
         </div>
-        <h3>{title}</h3>
-        <p>{message}</p>
-    </div>
-  )
+    )
 }
 
 export default Card
