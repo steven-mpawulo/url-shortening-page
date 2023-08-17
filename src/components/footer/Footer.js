10887b395903029f5977b26e8699ab4597405571
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-17 14:56:32 +0300

    works on second colum

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,8 +4,16 @@ import Logo from '../../images/logo.svg'
 const Footer = () => {
   return (
     <footer>
-        <div className="first-footer-column">
+        <div className="footer-first-column">
             <img src={Logo} alt="logo" />
+        </div>
+        <div className="footer-second-column">
+            <ul>
+                <li>Features</li>
+                <li><a href="#Link Shortening">Link Shortening</a></li>
+                <li><a href="#Branded Links">Branded Links</a></li>
+                <li><a href="#Analytics">Analytics</a></li>
+            </ul>
         </div>
     </footer>
   )
