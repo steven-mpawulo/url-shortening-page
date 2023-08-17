374d306833315a0adcfe2c61dc1722ceb7e790e2
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-17 11:23:55 +0300

    creates first section and partitions it into two containers

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Main = () => {
   return (
-    <div>Main</div>
+    <main>
+        <section className='first-section'>
+            <div className="first-section-left-container"></div>
+            <div className="first-section-right-container"></div>
+        </section>
+    </main>
   )
 }
 
