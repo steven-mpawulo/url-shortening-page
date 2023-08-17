737600db920ab8ddb7f26c8245e9b2a0909f8d8f
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-17 15:13:29 +0300

    adds footer to app.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import Main from "./components/main/Main";
 
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
       <Header/>
       <Main/>
+      <Footer/>
     </div>
   );
 }
